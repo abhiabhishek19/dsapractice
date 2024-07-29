d1c3f4190d8d54c5e071ccc6fadd0977eb5cc997
abhiabhishek19 <103142778+abhiabhishek19@users.noreply.github.com>
2024-07-29 19:17:45 +0530

    Time: 2 ms (84.41%), Space: 13.2 MB (5.51%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -9,24 +9,38 @@
  * };
  */
 class Solution {
+private:
+    void Solve(ListNode* &head, ListNode* prev,ListNode* curr )
+    {
+        if(curr==NULL)
+        {
+            head=prev;
+            return;
+        }
+        ListNode* forward = curr->next;
+        Solve(head,curr,forward);
+        curr->next = prev;
+    }
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* curr = head;
         ListNode* prev = NULL;
-        ListNode* forward = NULL;
-        if(head==NULL || head->next==NULL)
-        {
-            return head;
-        }
-        while(curr!=NULL)
-        {
-            forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
+        Solve(head,prev,curr);
+        return head;
+        // ListNode* forward = NULL;
+        // if(head==NULL || head->next==NULL)
+        // {
+        //     return head;
+        // }
+        // while(curr!=NULL)
+        // {
+        //     forward=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=forward;
             
-        }
-        return prev;
+        // }
+        // return prev;
         
     }
 };
